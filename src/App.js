eef1c50d44d6fad946426c6e1adccb688fc94e50
yostenalewis1 <yostenalewis9@gmail.com>
2023-12-06 00:19:04 +0200

    create header of the blog

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import logo from "./images/terminal-solid 1.svg";
+import rectangle from "./images/rectangles.svg";
+
 export default function App() {
   return (
     <div>
-     
+      <Header />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div className="container">
+      <img src={rectangle} alt="rec" className="rectangle" />
+      <img src={rectangle} alt="rec" className="rectangle1" />
+
+      <div className="Logo">
+        <img src={logo} alt="logo" className="logopic" />
+        <div className="text">GITHUB BLOG</div>
+      </div>
     </div>
   );
 }
